1f5c65996c9f9d83f26b4a4a69cc6237ecc2a8a5
AryanGandotra <aryan.gandotra.21cse@bmu.edu.in>
2023-07-16 00:46:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const Products = require("./models/product");
 require("dotenv").config();
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 
 const { PORT, DB_CONNECTION_STRING } = process.env;
 
@@ -11,6 +12,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "Public")));
+app.use(methodOverride("_method"));
 
 mongoose.connect(DB_CONNECTION_STRING, {
   useNewUrlParser: true,
@@ -33,10 +35,33 @@ app.get("/products", async (req, res) => {
   res.render("products/index", { products });
 });
 
+app.get("/products/new", (req, res) => {
+  res.render("products/new");
+});
+
+app.post("/products", async (req, res) => {
+  const newProduct = new Products(req.body.product);
+  await newProduct.save();
+  res.redirect(`/products/${newProduct._id}`);
+});
+
+app.put("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = await Products.findByIdAndUpdate(id, req.body.product);
+  await product.save();
+  res.redirect(`/products/${product._id}`);
+});
+
 app.get("/products/:id", async (req, res) => {
   const { id } = req.params;
   const product = await Products.findById(id);
   res.render("products/show", { product });
+});
+
+app.get("/products/:id/edit", async (req, res) => {
+  const { id } = req.params;
+  const product = await Products.findById(id);
+  res.render("products/edit", { product });
 });
 
 app.listen(PORT, () => {
