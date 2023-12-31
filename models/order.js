2aba7aa1143741846f37b7030043dc08e4f82107
AryanGandotra <aryan.gandotra.21cse@bmu.edu.in>
2023-12-31 23:39:20 +0530

    Update order.js

--- models/order.js
+++ models/order.js
@@ -18,3 +18,36 @@ const orderHistorySchema = new Schema(
 );
 
 module.exports = mongoose.model("OrderHistory", orderHistorySchema);
+
+// const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
+
+// const orderHistorySchema = new mongoose.Schema({
+//   user_id: {
+//     type: mongoose.Schema.Types.ObjectId,
+//     ref: "User",
+//     required: true,
+//   },
+//   order_date: { type: Date, default: Date.now },
+//   total_amount: Number,
+//   items: [itemSchema],
+//   shipping_address: {
+//     address_line1: String,
+//     address_line2: String,
+//     city: String,
+//     state: String,
+//     postal_code: String,
+//     country: String,
+//   },
+//   payment_info: {
+//     payment_method: String,
+//     transaction_id: String,
+//     payment_status: String,
+//     payment_date: Date,
+//   },
+//   order_status: String,
+//   shipment_tracking: String,
+//   order_notes: [noteSchema],
+// });
+
+// module.exports = mongoose.model("OrderHistory", orderHistorySchema);
