a21c49b726304841c9e511747d9438efd20a6176
AryanGandotra <aryan.gandotra.21cse@bmu.edu.in>
2024-01-02 16:16:56 +0530

    Update order.js

--- models/order.js
+++ models/order.js
@@ -3,11 +3,13 @@ const Schema = mongoose.Schema;
 
 const orderHistorySchema = new Schema(
   {
-    cart: {
-      type: Schema.Types.ObjectId,
-      ref: "Product",
-      required: [true, "Product is required"],
-    },
+    carts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Cart",
+        required: [true, "Cart is required"],
+      },
+    ],
     user: {
       type: Schema.Types.ObjectId,
       ref: "User",
@@ -17,37 +19,13 @@ const orderHistorySchema = new Schema(
   { timestamps: true }
 );
 
-module.exports = mongoose.model("OrderHistory", orderHistorySchema);
+orderHistorySchema.virtual("populatedCarts", {
+  ref: "Cart",
+  localField: "carts",
+  foreignField: "_id",
+  justOne: false,
+});
 
-// const mongoose = require("mongoose");
-// const Schema = mongoose.Schema;
+const OrderHistory = mongoose.model("OrderHistory", orderHistorySchema);
 
-// const orderHistorySchema = new mongoose.Schema({
-//   user_id: {
-//     type: mongoose.Schema.Types.ObjectId,
-//     ref: "User",
-//     required: true,
-//   },
-//   order_date: { type: Date, default: Date.now },
-//   total_amount: Number,
-//   items: [itemSchema],
-//   shipping_address: {
-//     address_line1: String,
-//     address_line2: String,
-//     city: String,
-//     state: String,
-//     postal_code: String,
-//     country: String,
-//   },
-//   payment_info: {
-//     payment_method: String,
-//     transaction_id: String,
-//     payment_status: String,
-//     payment_date: Date,
-//   },
-//   order_status: String,
-//   shipment_tracking: String,
-//   order_notes: [noteSchema],
-// });
-
-// module.exports = mongoose.model("OrderHistory", orderHistorySchema);
+module.exports = OrderHistory;
