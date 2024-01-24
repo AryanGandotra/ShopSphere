470b68b480b50375a1da9177ba1138384bdcfca8
AryanGandotra <aryan.gandotra.21cse@bmu.edu.in>
2024-01-25 00:41:44 +0530

    removed session and added jwt

--- middleware.js
+++ middleware.js
@@ -1,27 +1,62 @@
-// Function for checking if user is logged in or not
+// // Function for checking if user is logged in or not
 
+// module.exports.isLogged = (req, res, next) => {
+//   // if (!req.isAuthenticated()) {
+//   //   req.session.returnTo = req.originalUrl;
+//   //   return res.redirect("/login");
+//   // } else {
+//   //   next();
+//   // }
+// };
+
+// // Function for storing the url the user is requesting
+
+// module.exports.storeReturnTo = (req, res, next) => {
+//   if (req.session.returnTo) {
+//     res.locals.returnTo = req.session.returnTo;
+//   }
+//   next();
+// };
+
+// // Function for login in the user and redirecting to the url they were requesting if no url is stored redirecting to home page
+
+// module.exports.login = (req, res) => {
+//   const redirectUrl = res.locals.returnTo || "/";
+//   delete res.locals.returnTo;
+//   res.redirect(redirectUrl);
+// };
+
+const jwt = require("jsonwebtoken");
+
+// Function for checking if user is logged in or not
 module.exports.isLogged = (req, res, next) => {
-  if (!req.isAuthenticated()) {
-    req.session.returnTo = req.originalUrl;
-    return res.redirect("/login");
-  } else {
-    next();
+  const token = req.headers["authorization"];
+  if (!token) {
+    return res.status(401).json({ error: "No token provided" });
   }
+
+  jwt.verify(token, "your_secret_key", (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ error: "Failed to authenticate token" });
+    }
+
+    // Store the decoded payload in the request
+    req.user = decoded;
+    next();
+  });
 };
 
 // Function for storing the url the user is requesting
-
 module.exports.storeReturnTo = (req, res, next) => {
-  if (req.session.returnTo) {
-    res.locals.returnTo = req.session.returnTo;
+  if (req.user && req.user.returnTo) {
+    res.locals.returnTo = req.user.returnTo;
   }
   next();
 };
 
 // Function for login in the user and redirecting to the url they were requesting if no url is stored redirecting to home page
-
 module.exports.login = (req, res) => {
   const redirectUrl = res.locals.returnTo || "/";
   delete res.locals.returnTo;
   res.redirect(redirectUrl);
-};
+}; 
